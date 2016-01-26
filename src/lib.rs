4516290bf5aac194c3af0a156b7d506990366c55
Snirk Immington <snirk.immington@gmail.com>
2016-01-26 12:04:20 -0500

    Fixed ffi types

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ extern crate libc;
 use std::option::Option;
 use std::env;
 use std::ffi;
+use std::os::unix::prelude;
+
 
 // Types
 
@@ -274,11 +276,6 @@ pub struct InputInterface {
 //#[repr(C)]
 pub enum LibinputDevice {}
 
-/// Represents a wlc resource, which represents a wayland surface.
-/// This object can be queried for its size wayland surface properties
-/// and rendered in pre and post render hooks.
-type WLCResource = libc::uintptr_t;
-
 /// Represents a wayland display.
 enum WLDisplay { }
 
@@ -291,7 +288,7 @@ enum WLResource { }
 extern "C" {
     fn wlc_exec(bin: *const libc::c_char, args: *const *const libc::c_char) -> ();
 
-    fn wlc_init(interface: *const WlcInterface, argc: i32, argv: *const *const char) -> bool;
+    fn wlc_init(interface: *const WlcInterface, argc: i32, argv: *const *const libc::c_char) -> bool;
 
     /// Runs WLC run loop;
     fn wlc_run() -> ();
@@ -304,11 +301,13 @@ extern "C" {
 /// true, continue with `rustwlc::run_wlc()` to run wlc's event loop.
 pub fn init(interface: WlcInterface) -> bool {
     unsafe {
-        let args: Vec<String> = env::args().collect();
-        let argc = args.len();
+        let argv: Vec<ffi::CString> = env::args().into_iter()
+            .map(|arg| ffi::CString::new(arg).unwrap() ).collect();
 
-        //let args_c =
-        true
+        let args: Vec<*const libc::c_char> = argv.into_iter()
+            .map(|arg: ffi::CString| { arg.as_ptr() as *const libc::c_char }).collect();
+
+        wlc_init(&interface, args.len() as i32, args.as_ptr() as *const *const libc::c_char)
     }
 }
 
