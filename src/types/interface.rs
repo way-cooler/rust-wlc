9c8c19b402946b10aee6f9fb8f0ca34531e34028
snirk <snirk.immington@gmail.com>
2016-01-30 15:59:39 -0500

    Made interface handlers Option callbacks

--- src/types/interface.rs
+++ src/types/interface.rs
@@ -1,4 +1,4 @@
-//! types/interface
+://! types/interface
 //! Contains all structs used for initializing wlc.
 //! You will only need this module for invoking
 //! `rustwlc::init`.
@@ -7,6 +7,7 @@
 extern crate libc;
 
 use std::ffi;
+use std::option::Option;
 use std::ffi::{CString};
 
 use super::*;
@@ -31,12 +32,12 @@ pub struct WlcInterface {
 #[repr(C)]
 pub struct OutputInterface {
     /// Output was created
-    pub created: extern fn(handle: WlcOutput) -> bool,
+    pub created: Option<extern "C" fn(handle: WlcOutput) -> bool>,
     /// Output lost or destroyed
-    pub destroyed: extern fn(handle: WlcOutput),
-    pub focus: extern fn(handle: WlcOutput, focused: bool),
+    pub destroyed: Option<extern "C" fn(handle: WlcOutput)>,
+    pub focus: Option<extern "C" fn(handle: WlcOutput, focused: bool)>,
     /// Output resolution changed
-    pub resolution: extern fn(handle: WlcOutput, old_size: Size, new_size: Size),
+    pub resolution: Option<extern "C" fn(handle: WlcOutput, old_size: Size, new_size: Size)>,
     pub render: OutputRenderInterface,
 }
 
@@ -44,9 +45,9 @@ pub struct OutputInterface {
 #[repr(C)]
 pub struct OutputRenderInterface {
     /// Pre render hook
-    pub pre: extern fn(handle: WlcOutput),
+    pub pre: Option<extern "C" fn(handle: WlcOutput)>,
     /// Post render hook
-    pub post: extern fn(handle: WlcOutput),
+    pub post: Option<extern "C" fn(handle: WlcOutput)>,
 }
 
 /// Represents window viewing callbacks
@@ -54,13 +55,13 @@ pub struct OutputRenderInterface {
 pub struct ViewInterface {
     /// View was created. Return false if you want to destroy the view
     /// (e.g. failed to allocated data related to the view)
-    pub created: extern fn(handle: WlcView) -> bool,
+    pub created: Option<extern "C" fn(handle: WlcView) -> bool>,
     /// View was destroyed
-    pub destroyed: extern fn(handle: WlcView),
+    pub destroyed: Option<extern "C" fn(handle: WlcView)>,
     /// View lost or got focus
-    pub focus: extern fn(handle: WlcView, focused: bool),
+    pub focus: Option<extern "C" fn(handle: WlcView, focused: bool)>,
     /// View was moved to to output
-    pub move_to_output: extern fn(current: WlcView, from_output: WlcView, to_output: WlcView),
+    pub move_to_output: Option<extern "C" fn(current: WlcView, from_output: WlcView, to_output: WlcView)>,
     pub request: RequestInterface,
 }
 
@@ -69,15 +70,15 @@ pub struct ViewInterface {
 pub struct RequestInterface {
     /// Request to set given geometry to view. Apply using
     /// wlc_view_set_geometry (handle.set_geometry) to agree.
-    pub geometry: extern fn(handle: WlcView, geometry: Geometry),
+    pub geometry: Option<extern "C" fn(handle: WlcView, geometry: Geometry)>,
     /// Request to disable or enable the given state for a view.
     /// Apply using wlc_view_set_state to agree.
-    pub state: extern fn(current: WlcView, state: ViewState, handled: bool),
+    pub state: Option<extern "C" fn(current: WlcView, state: ViewState, handled: bool)>,
     /// View requests to move itself. Start an interactive move to agree.
-    pub move_: extern fn(handle: WlcView, destination: Point),
+    pub move_: Option<extern "C" fn(handle: WlcView, destination: Point)>,
     /// Request to resize itself with the given edges.
     /// Start and interactive move to agree
-    pub resize: extern fn(handle: WlcView, edge: ResizeEdge, location: Point),
+    pub resize: Option<extern "C" fn(handle: WlcView, edge: ResizeEdge, location: Point)>,
 
     /// View rendering hooks
     pub render: ViewRenderInterface,
@@ -86,8 +87,8 @@ pub struct RequestInterface {
 /// Represents rendering callbacks for views
 #[repr(C)]
 pub struct ViewRenderInterface {
-    pub pre: extern fn(view: WlcView),
-    pub post: extern fn(view: WlcView)
+    pub pre: Option<extern "C" fn(view: WlcView)>,
+    pub post: Option<extern "C" fn(view: WlcView)>
 }
 
 /// Represents keyboard press callbacks
@@ -95,7 +96,7 @@ pub struct ViewRenderInterface {
 pub struct KeyboardInterface {
     /// Key event was triggered, view.0 will be zero if there was no focus
     /// Return true to prevent sending the event to clients.
-    pub key: extern fn(view: WlcView, time: u32, mods: KeyboardModifiers, key: u32, state: KeyState) -> bool,
+    pub key: Option<extern "C" fn(view: WlcView, time: u32, mods: KeyboardModifiers, key: u32, state: KeyState) -> bool>,
 }
 
 /// Represents mouse input callbacks
@@ -103,14 +104,14 @@ pub struct KeyboardInterface {
 pub struct PointerInterface {
     /// Button event was triggered, handle.0 will be zero if there was no
     /// focus. Return true to prevent sending the event to clients.
-    pub button: extern fn(hande: WlcView, button: libc::c_uint, mods: KeyboardModifiers, time: u32, state: ButtonState, point: Point) -> bool,
+    pub button: Option<extern "C" fn(hande: WlcView, button: libc::c_uint, mods: KeyboardModifiers, time: u32, state: ButtonState, point: Point) -> bool>,
     /// Scroll event was triggered, view handle will be zero if there was
     /// no focus. Return true to prevent sending the event to clients.
-    pub scroll: extern fn(handle: WlcView, button: u32, mods: KeyboardModifiers, axis: ScrollAxis, amount: [u64; 2]) -> bool,
+    pub scroll: Option<extern "C" fn(handle: WlcView, button: u32, mods: KeyboardModifiers, axis: ScrollAxis, amount: [u64; 2]) -> bool>,
     /// Mouse was moved, handle.0 will be zero if there was no focus.
     /// Use wlc_pointer_set_position to agree. Return true to prevent
     /// sending event to clients.
-    pub motion: extern fn(heights: WlcView, dist: u32, point: Point),
+    pub motion: Option<extern "C" fn(heights: WlcView, dist: u32, point: Point)>,
 }
 
 /// Represents touchscreen callbacks
@@ -118,19 +119,19 @@ pub struct PointerInterface {
 pub struct TouchInterface {
     /// Screen was touched, handle.0 will be zero if there was no focus.
     /// Return true to prevent sending the event to clients.
-    pub touch: extern fn(handle: WlcView, time: libc::c_uint, mods: KeyboardModifiers, touch: TouchType, key: libc::c_int, point: Point) -> bool,
+    pub touch: Option<extern "C" fn(handle: WlcView, time: libc::c_uint, mods: KeyboardModifiers, touch: TouchType, key: libc::c_int, point: Point) -> bool>,
 }
 
 /// Represents a callback for initializing the callback
 #[repr(C)]
 pub struct CompositorInterface {
     /// Compositor is ready to accept clients.
-    pub ready: extern fn()
+    pub ready: Option<extern "C" fn()>
 }
 
 /// Represents experimencallbacks for libinput events
 #[repr(C)]
 pub struct InputInterface {
-    pub created: extern fn(device: LibinputDevice) -> bool,
-    pub destroyed: extern fn(device: LibinputDevice)
+    pub created: Option<extern "C" fn(device: LibinputDevice) -> bool>,
+    pub destroyed: Option<extern "C" fn(device: LibinputDevice)>
 }
