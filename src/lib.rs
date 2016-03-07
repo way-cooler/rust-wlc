83574d5b4ec5f98b5899e025440adcbb9b501e81
Snirk Immington <snirk.immington@gmail.com>
2016-03-07 11:54:44 -0800

    Fix lib.rs doctests

--- src/lib.rs
+++ src/lib.rs
@@ -44,14 +44,18 @@ extern "C" {
 ///
 /// # Example
 /// ```no_run
+/// use rustwlc;
+///
 /// let interface = rustwlc::interface::WlcInterface::new();
 /// // Set a default log callback
 /// rustwlc::log_set_default_handler();
 ///
-/// if !rustwlc::init(interface) {
-///      panic!("Unable to init");
+/// if let Some(run_wlc) = rustwlc::init(interface) {
+///      run_wlc()
 /// }
-/// rustwlc::run_wlc();
+/// else {
+///      panic!("Unable to initialize wlc!");
+/// }
 /// ```
 pub fn init(interface: WlcInterface) -> Option<fn() -> ()> {
     unsafe {
@@ -68,27 +72,17 @@ pub fn init(interface: WlcInterface) -> Option<fn() -> ()> {
 /// The initialize functions will return this function in an Option.
 /// If and only if they succeed can this function be called wlc with `rustwlc::init` call this method
 /// to being wlc's main event loop.
-///
-/// # Example
-/// ```no_run
-/// use rustwlc;
-///
-/// let interface = rustwlc::interface::WlcInterface::new();
-/// rustwlc::log_set_default_handler();
-///
-/// if !rustwlc::init(interface) {
-///      panic!("Unable to init");
-/// }
-/// rustwlc::run_wlc();
-/// ```
 fn run_wlc() {
     unsafe {
         wlc_run();
     }
 }
 
-/// Executes a program in wayland.
-/// Is passed the program and all arguments (the first should be the program)
+/// Deprecated, do not use.
+///
+/// # Deprecated
+/// This function does not seem to work across the FFI boundary, and Rust provides a
+/// much better interface in the `std::command::Command` class to executing programs.
 pub fn exec(bin: String, args: Vec<String>) {
     unsafe {
         let bin_c = CString::new(bin).unwrap().as_ptr() as *const libc::c_char;
@@ -114,7 +108,22 @@ pub fn terminate() {
     }
 }
 
-/// Registers a callback for wlc logging
+/// Registers a callback for wlc logging.
+///
+/// Note that `rustwlc::log_set_default_handler()` will register a simple callback
+/// that will print the type and text to the console.
+///
+/// # Parameters
+/// The `handler` callback has two parameters:
+/// * `type`: The `LogType` of the message being printed.
+/// * `text`: The text to be logged, currently in C form. One may call `rustwlc::pointer_to_string`
+/// to convert it to a Rust String.
+///
+/// # Safety
+/// The callback function (like other callbacks in `rustwlc`) must be marked as extern as it is called
+/// from C code.
+///
+/// In addition, `unsafe` will be required to convert the text into a Rust String.
 #[allow(dead_code)]
 pub fn log_set_handler(handler: extern "C" fn(type_: LogType, text: *const libc::c_char)) {
     unsafe {
@@ -130,24 +139,40 @@ extern "C" fn default_log_callback(log_type: LogType, text: *const libc::c_char)
 
 /// Sets the wlc log callback to a simple function that prints to console.
 ///
-/// Not calling any log_set_handler will have no logging, use this or
-/// log_set_handler with a callback to use wlc logging.
+/// Not calling any `log_set_handler` will have no logging, use this or
+/// `log_set_handler` with a callback to use wlc logging.
 ///
 /// # Example
 /// ```no_run
+/// use rustwlc;
+///
 /// let interface = rustwlc::interface::WlcInterface::new();
 /// rustwlc::log_set_default_handler();
-/// if !rustwlc::init(interface) {
-///      panic!("Unable to init");
+///
+/// if let Some(run_wlc) = rustwlc::init(interface) {
+///      run_wlc();
 /// }
-/// rustwlc::run_wlc();
+/// else {
+///     panic!("Unable to initialize wlc!");
+/// }
 /// ```
 pub fn log_set_default_handler() {
     log_set_handler(default_log_callback);
 }
 
+/// Unsafe strings conversion function.
+///
 /// Converts a `*const libc::c_char` to an owned `String`.
 /// Useful for log callbacks.
+///
+/// # Example
+/// Standard usage may be for the log callbacks.
+/// ```rust
+/// extern "C" fn default_log_callback(log_type: LogType, text: *const libc::c_char) {
+///     let string = unsafe { pointer_to_string(text) };
+///     println!("wlc [{:?}]: {}", log_type, string);
+/// }
+/// ```
 pub unsafe fn pointer_to_string(pointer: *const libc::c_char) -> String {
     let slice = ffi::CStr::from_ptr(pointer);
     slice.to_string_lossy().into_owned()
