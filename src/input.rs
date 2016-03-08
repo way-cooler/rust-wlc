c0f907cc5758f7523408ad1bda2066e181e7f925
Timidger <APragmaticPlace@gmail.com>
2016-03-08 14:33:37 -0500

    Changed get_current_keys to return a vector, from a slice that was looking into memory we don't own

--- src/input.rs
+++ src/input.rs
@@ -48,11 +48,15 @@ pub mod keyboard {
     use std::slice;
 
     /// Get currently held keys.
-    pub fn get_current_keys<'a>() -> &'a[u32] {
+    pub fn get_current_keys() -> Vec<u32> {
         let mut out_memb: size_t = 0;
         unsafe {
             let keys = super::wlc_keyboard_get_current_keys(&mut out_memb);
-            return slice::from_raw_parts(keys, out_memb as usize);
+            let mut result = Vec::with_capacity(out_memb);
+            for index in (0 as isize) .. (out_memb as isize) {
+                result.push(*(keys.offset(index)));
+            }
+            result
         }
     }
 
