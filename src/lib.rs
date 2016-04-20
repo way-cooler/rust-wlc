2eacf41ce80d9cf00620ef8191b5f06798cf162c
Timidger <APragmaticPlace@gmail.com>
2016-04-20 18:16:38 -0400

    Switched to &str reference

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub mod xkb;
 pub use types::*;
 
 // Log Handler hack
-static mut rust_logging_fn: fn(_type: LogType, string: String) = default_log_callback;
+static mut rust_logging_fn: fn(_type: LogType, string: &str) = default_log_callback;
 
 // External WLC functions
 #[link(name = "wlc")]
@@ -139,21 +139,21 @@ pub fn terminate() {
 /// from C code.
 ///
 /// In addition, `unsafe` will be required to convert the text into a Rust String.
-pub fn log_set_handler(handler: fn(type_: LogType, text: String)) {
+pub fn log_set_handler(handler: fn(type_: LogType, text: &str)) {
     unsafe {
         // Set global handler function
         rust_logging_fn = handler;
         extern "C" fn c_handler(type_: LogType, text: *const libc::c_char) {
             unsafe {
                 let string = ffi::CStr::from_ptr(text).to_string_lossy().into_owned();
-                rust_logging_fn(type_, string);
+                rust_logging_fn(type_, &string);
             }
         }
         wlc_log_set_handler(c_handler);
     }
 }
 
-fn default_log_callback(log_type: LogType, text: String) {
+fn default_log_callback(log_type: LogType, text: &str) {
     println!("wlc [{:?}] {}", log_type, text);
 }
 
