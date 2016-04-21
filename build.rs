361b190e52abcadfaf32fa6ab161ddad36f29b52
Timidger <APragmaticPlace@gmail.com>
2016-04-20 23:24:49 -0400

    --amend

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let libs = &[(Some("../wlc/target/src/"), "wlc")];
-    for &(ref m_path, ref lib) in libs {
-        if let &Some(static_path) = m_path {
-            println!("cargo:rustc-link-search={}", &static_path);
-        }
-        println!("cargo:rustc-link-lib={}", &lib);
-    }
-}
