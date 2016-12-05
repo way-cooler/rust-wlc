6788012f0c86d74e583a94c5806d51348766ff94
Timidger <APragmaticPlace@gmail.com>
2016-12-04 23:42:44 -0500

    Added most other dependencies
    Still need to fix chck, and also need to some how specify the unstable
    pre-built protocols (eg xdg-shell)...

--- build.rs
+++ build.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("cargo:rustc-link-search=/usr/local/lib64")
+    println!("cargo:rustc-link-search=/usr/local/lib64");
+    println!("cargo:rustc-link-lib=dylib=wayland-client");
+    println!("cargo:rustc-link-lib=dylib=wayland-server");
+    println!("cargo:rustc-link-lib=dylib=systemd");
+    println!("cargo:rustc-link-lib=dylib=input");
+    println!("cargo:rustc-link-lib=dylib=udev");
+    println!("cargo:rustc-link-lib=dylib=GLESv2");
+    println!("cargo:rustc-link-lib=dylib=drm");
+    println!("cargo:rustc-link-lib=dylib=gbm");
+    println!("cargo:rustc-link-lib=dylib=xcb");
+    println!("cargo:rustc-link-lib=dylib=xcb-composite");
+    println!("cargo:rustc-link-lib=dylib=xcb-ewmh");
+    println!("cargo:rustc-link-lib=dylib=xcb-xkb");
+    println!("cargo:rustc-link-lib=dylib=xcb-image");
+    println!("cargo:rustc-link-lib=dylib=xcb-xfixes");
+    println!("cargo:rustc-link-lib=dylib=pixman-1");
+    println!("cargo:rustc-link-lib=dylib=X11");
+    println!("cargo:rustc-link-lib=dylib=X11-xcb");
+    println!("cargo:rustc-link-lib=dylib=EGL");
+    println!("cargo:rustc-link-search=native=/usr/include")
 }
