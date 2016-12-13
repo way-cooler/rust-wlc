166648ee23a50eb51ee24b6f821b0ca7ddf57af3
Timidger <APragmaticPlace@gmail.com>
2016-12-12 23:28:57 -0500

    Wrapped read and write pixels

--- src/render.rs
+++ src/render.rs
@@ -8,18 +8,18 @@ use super::types::{Geometry};
 #[cfg_attr(not(feature = "static-wlc"), link(name = "wlc"))]
 
 #[repr(C)]
-enum wlc_pixel_format {
+pub enum wlc_pixel_format {
     WLC_RGBA8888
 }
 
 #[repr(C)]
-enum wlc_renderer {
+pub enum wlc_renderer {
     WLC_RENDERER_GLES2,
     WLC_NO_RENDERER
 }
 
 #[repr(C)]
-enum wlc_surface_format {
+pub enum wlc_surface_format {
     SURFACE_RGB,
     SURFACE_RGBA,
     SURFACE_EGL,
@@ -30,7 +30,7 @@ enum wlc_surface_format {
 
 extern "C" {
     /// Write pixel data with the specific format to output's framebuffer.
-    /// If the geometry is out of bounds, it will be automaticall clamped.
+    /// If the geometry is out of bounds, it will be automatically clamped.
     fn wlc_pixels_write(format: wlc_pixel_format, geometry: *const Geometry, data: *const c_void);
 
     /// Read pixel data from output's framebuffer.
@@ -61,4 +61,33 @@ extern "C" {
     fn wlc_surface_get_textures(surface: uintptr_t,
                                 out_textures: *mut uint32_t,
                                 out_format: *mut wlc_surface_format);
+}
+
+
+/// Write pixel data with the specific format to output's framebuffer.
+/// If the geometry is out of bounds, it will be automatically clamped.
+///
+/// # Unsafety
+/// The data is converted to a *mut c_void and then passed to C to read.
+/// It should be in the format expected by wlc, see the wlc docs.
+pub fn write_pixels<T>(format: wlc_pixel_format, geometry: Geometry, data: &mut T) {
+    unsafe {
+        let data = data as *mut _ as *mut c_void;
+        wlc_pixels_write(format, &geometry as *const Geometry, data);
+    }
+}
+
+/// Reads the pixels at the specified geometry
+pub fn read_pixels(format: wlc_pixel_format, mut geometry: Geometry) -> ([u8; 9], Vec<u8>) {
+    let data_size = (geometry.size.w * geometry.size.h * 4) as usize;
+    // magic response header size
+    let header_size = 9;
+    let mut out_buf: Vec<u8> = Vec::with_capacity(header_size + data_size);
+    unsafe {
+        wlc_pixels_read(format, &mut geometry as *mut _, out_buf.as_mut_ptr() as *mut c_void);
+    }
+    let mut header_response = [0u8; 9];
+    let response: Vec<u8> = out_buf.drain(0..9).collect();
+    header_response.copy_from_slice(response.as_slice());
+    (header_response, out_buf)
 }
